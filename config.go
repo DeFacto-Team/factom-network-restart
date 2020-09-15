f58f205cab982bec86f9ebce20b2f16ed3045ae1
ilzheev <anton@lovedesign.ru>
2020-09-15 13:49:33 +0400

    remove extra yaml parsing

--- config.go
+++ config.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
-
-	"github.com/go-yaml/yaml"
 	"github.com/jinzhu/configor"
 	"github.com/mcuadros/go-defaults"
 )
@@ -21,15 +18,7 @@ func NewConfig(configFile string) (*Config, error) {
 	config := new(Config)
 	defaults.SetDefaults(config)
 
-	configBytes, err := ioutil.ReadFile(configFile)
-	if err == nil {
-		err = yaml.Unmarshal(configBytes, &config)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if err := configor.Load(config); err != nil {
+	if err := configor.Load(config, configFile); err != nil {
 		return nil, err
 	}
 	return config, nil
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/DeFacto-Team/factom-network-restart
 go 1.14
 
 require (
-	github.com/go-yaml/yaml v2.1.0+incompatible
 	github.com/jinzhu/configor v1.2.0
 	github.com/mcuadros/go-defaults v1.2.0
 )
